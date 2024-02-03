be9a781b31528ae9bb7e9641202605376ede4be4
Muhammad Sony Setiawan <132990489+MuhammadSonySetiawan@users.noreply.github.com>
2024-02-03 17:41:08 +0700

    []

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import "bootstrap/dist/js/bootstrap.bundle"
+import "bootstrap/dist/css/bootstrap.min.css"
+function App() {
+  return (
+    <div className="App">
+     hallo
+     <button type="button" class="btn btn-warning">Warning</button>
+    </div>
+  );
+}
+
+export default App;
